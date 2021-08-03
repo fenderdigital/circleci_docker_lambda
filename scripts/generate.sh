56846e9068a1c3fa32e87741dbb5d6e76b202945
KP <kkp@fender.com>
2021-08-03 13:03:59 -0700

    Update node version to use

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -30,7 +30,9 @@ if [ ! -e "$NODE_VERSIONS_NUM" ] ; then
     do
       echo "RUN . /root/.nvm/nvm.sh && nvm install $NODE_VERSION"
     done
-    echo "RUN . /root/.nvm/nvm.sh && nvm use 16.6.1 && npm install -g serverless"
+    echo "RUN . /root/.nvm/nvm.sh && \
+    nvm use 14.17.4 && \
+    npm install -g serverless"
 fi
 
 if [ $JAVA = "true" ] ; then
